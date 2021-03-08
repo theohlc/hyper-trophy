ae9c6865e05bea80ab201d035b3dd0d940cda915
theo <teddyhoover@gmail.com>
2021-03-08 15:58:27 -0600

    makes password for google accounts

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   devise :omniauthable, omniauth_providers: [:google_oauth2]
 
   def self.from_google(email:, full_name:, uid:, avatar_url:)
-    create_with(uid: uid, full_name: full_name, avatar_url: avatar_url).find_or_create_by!(email: email)
+    password = Devise.friendly_token
+    create_with(password: password, uid: uid, full_name: full_name, avatar_url: avatar_url).find_or_create_by!(email: email)
   end
 end
