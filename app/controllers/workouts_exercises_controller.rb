4019549c197ec6a9f466b96d2380fa3868266f45
theo <teddyhoover@gmail.com>
2021-03-10 13:01:24 -0600

    added WorkoutsExercises update

--- app/controllers/workouts_exercises_controller.rb
+++ app/controllers/workouts_exercises_controller.rb
@@ -4,10 +4,16 @@ class WorkoutsExercisesController < ApplicationController
   end
 
   def update
-    binding.pry
     @exercise_name = params[:workouts_exercise][:exercise_name].capitalize
     @exercise =  Exercise.find_or_create_by(name: @exercise_name)
-    @workouts_exercise = find(params[:workouts_exercise][:id])
+    @workouts_exercise = Workouts_Exercise.find(params[:workouts_exercise][:id])
     
+    @workouts_exercise.update(
+      reps:         params[:workouts_exercise][:reps]
+      sets:         params[:workouts_exercise][:sets]
+      exercise_id:  params[:workouts_exercise][:exercise_id]
+    )
+
+    next_exercise
   end
 end
