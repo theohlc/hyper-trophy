26e578b311777c96074656456b9d53150f5a3b2e
theo <teddyhoover@gmail.com>
2021-03-10 12:33:09 -0600

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_10_172720) do
+ActiveRecord::Schema.define(version: 2021_03_10_182346) do
+
+  create_table "exercises", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
