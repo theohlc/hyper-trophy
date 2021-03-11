d6980b7fc1c39f68e37aa47774792236e34f6b1d
theo <teddyhoover@gmail.com>
2021-03-11 11:43:30 -0600

    fixed update method

--- app/controllers/workouts_exercises_controller.rb
+++ app/controllers/workouts_exercises_controller.rb
@@ -6,23 +6,27 @@ class WorkoutsExercisesController < ApplicationController
   end
 
   def update
-    @exercise_name = params[:workouts_exercise][:exercise_name].capitalize
-    @exercise =  Exercise.find_or_create_by(name: @exercise_name)
+    exercise_name = params[:workouts_exercise][:exercise_name].capitalize
+    exercise =  Exercise.find_or_create_by(name: exercise_name)
     @workouts_exercise = WorkoutsExercise.find(params[:id])
     
     @workouts_exercise.update(
       reps:         params[:workouts_exercise][:reps],
       sets:         params[:workouts_exercise][:sets],
-      exercise_id:  params[:workouts_exercise][:exercise_id]
+      exercise_id:  exercise.id
     )
-
+    
     redirect_to helpers.next_exercise(@workouts_exercise)
   end
 
   def new
-    workout = Workout.find(params[:workout_id])
+    @workout = Workout.find(params[:workout_id])
     @exercise_name = nil
-    @workouts_exercise = workout.workouts_exercises.build(order_occurs: (workout.workouts_exercises.last.order_occurs + 1))
+    @workouts_exercise = @workout.workouts_exercises.build(order_occurs: (@workout.workouts_exercises.last.order_occurs + 1))
+  end
+
+  def create
+    workouts_exercise = params[:workouts_exercise]
   end
 
 end
