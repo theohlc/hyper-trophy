7a736ff31dae33c532ba6450fa03decab5f768f6
theo <teddyhoover@gmail.com>
2021-03-11 11:09:18 -0600

    added links on workout page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :workouts do
-    resources :workouts_exercises, only: [ :edit, :update ]
+    resources :workouts_exercises, only: [ :new, :create, :edit, :update ]
   end
 
   root to: 'dashboards#show'
