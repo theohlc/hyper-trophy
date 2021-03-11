d754223099e44042ab55a0ee85a401ee02874616
theo <teddyhoover@gmail.com>
2021-03-11 13:45:49 -0600

    added count of exercises to users#show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
         @workouts = @user.workouts
+        @exercises = @user.exercises
     end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,16 @@ class User < ApplicationRecord
     password = Devise.friendly_token
     create_with(password: password, uid: uid, full_name: full_name, avatar_url: avatar_url).find_or_create_by!(email: email)
   end
+
+  def exercises
+    exercises = []
+
+    self.workouts.each do |workout|
+        workout.exercises.each do |exercise|
+          exercises << exercise
+        end
+    end
+    exercises.uniq
+  end
+
 end
