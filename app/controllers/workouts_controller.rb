81161b1ce532182a98d3aadc017482734c07f0e5
theo <teddyhoover@gmail.com>
2021-03-12 11:06:44 -0600

    added error messages

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -5,13 +5,11 @@ class WorkoutsController < ApplicationController
   end
 
   def create
-    binding.pry
     @workout = current_user.workouts.new(workout_params)
     if @workout.save
       for i in 1..@workout.num_exercises do
         @workout.workouts_exercises.create(order_occurs: i, exercise_id: 1)
       end
-      binding.pry
       redirect_to edit_workout_workouts_exercise_path(@workout, @workout.first_exercise)
     else
       render :new
